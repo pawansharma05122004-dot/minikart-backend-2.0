c2ba7f0ea1fe17dba5fe491d84df3db4d90e6020
Ta-Run <tarnsharma11091999@gmail.com>
2024-03-12 19:04:43 +0530

    add index path in vercel json

--- src/dbConnection/dbconnect.js
+++ src/dbConnection/dbconnect.js
@@ -3,12 +3,8 @@ import mongoose, { connect } from "mongoose";
 const dbConnect=async()=>{
     try{
      await mongoose.connect(process.env.MONGO_ATLAS_URL)
-
     }catch(err){
         console.log(err)
     }
-
-
-
 }
 export default dbConnect;
