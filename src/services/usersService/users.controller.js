52ab8bf0ed83a0153eb7ec390a47a53ae7ccec6f
Ta-Run <tarnsharma11091999@gmail.com>
2024-03-14 02:55:29 +0530

    add render production

--- src/services/usersService/users.controller.js
+++ src/services/usersService/users.controller.js
@@ -1,11 +1,15 @@
 import { UserModel } from "./users.model.js";
+import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import matches from "express-validator";
+// import verifyToken from '../middleware/authMiddleware.js'
 
 const signUp = async (req, res) => {
     try {
-        console.log(req)
         const { name, email, password, customer, phone_number } = req.body
-        const user = new UserModel({ name, email, customer, password: password, phone_number });
+        const hashPassword = await bcrypt.hash(password, 10)
+        //    const strongPassword = hashPassword.matches((/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/, "i").withMessage('Password should be combination of one uppercase , one lower case, one special char, one digit and min 8 , max 20 char long'))
+        const user = new UserModel({ name, email, customer, password: hashPassword, phone_number });
         await user.save()
         res.status(200).json({ success: true, result: user })
     } catch (error) {
@@ -23,9 +27,14 @@ const logIn = async (req, res) => {
         if (!user) {
             return res.status(401).json({ err: 'Authontication Field' })
         }
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+            return res.status(401).json({ err: 'Invalid Password' })
+        }
         const token = jwt.sign({ userId: user._id }, 'tarun@1234', {
             expiresIn: '1h'
         })
+
 
         res.cookie('token',token, { httpOnly: true, secure: true, maxAge: 3600000 })
         res.status(200).json({
@@ -33,7 +42,6 @@ const logIn = async (req, res) => {
                 user: user
             }
         })
-
     } catch (error) {
         res.status(500).json({
             mesaage: error.message
